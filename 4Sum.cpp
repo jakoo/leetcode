b5bf8feef129bf85d83fd333921127975991ddb3
Jako <zhang.jako@gmail.com>
2015-11-09 23:29:46 +0800

    update 4Sum.cpp

--- 4Sum.cpp
+++ 4Sum.cpp
@@ -11,12 +11,21 @@ public:
     
     void coreKSum(vector<int>& nums, int target, int k, vector<int> numArray, int begin, vector<vector<int>>& result)
     {
+        int sum = 0;
+        for (int i = 0; i < k; ++i)
+        {
+            sum += nums[begin + i];
+        }
+        if (sum > target)
+            return;
+            
         if (k == 2)
         {
             int end = nums.size() - 1;
+            sum = 0;
             while (begin < end)
             {
-                int sum = nums[begin] + nums[end];
+                sum = nums[begin] + nums[end];
                 if (sum == target)
                 {
                     vector<int> tmp(numArray);
@@ -44,6 +53,17 @@ public:
         numArray.push_back(0);
         for(int i = begin; i <= nums.size() - k;)
         {
+            sum = nums[i];
+            for (int j = 1; j < k; ++j)
+            {
+                sum += nums[nums.size() - j];
+            }
+            if (sum < target)
+            {
+                ++i;
+                while (i <= nums.size() - k && nums[i] == nums[i - 1]) ++i;
+                continue;
+            }
             numArray.pop_back();
             numArray.push_back(nums[i]);
             coreKSum(nums, target - nums[i], k - 1, numArray, i + 1, result);
